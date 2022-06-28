51410fc5211354274cc3aec464591a9527f26094
Talilotu <nesikaufusi@gmail.com>
2022-06-28 16:56:59 +1000

    Added ExpenseDate component CSS

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -8,9 +8,9 @@ function ExpenseDate(props) {
 
   return (
     <div className="ExpenseDate">
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <div className="month">{month}</div>
+      <div className="year">{year}</div>
+      <div className="day">{day}</div>
     </div>
   );
 }
