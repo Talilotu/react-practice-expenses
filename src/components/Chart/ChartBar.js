3376e338f9c9ee67fc2f50ac8d219bc5f47c7f02
Talilotu <nesikaufusi@gmail.com>
2022-06-30 23:50:59 +1000

    updated chartBar maxValue name

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,7 +5,7 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
@@ -14,7 +14,6 @@ const ChartBar = (props) => {
       <div className="chart-bar_inner">
         <div className="chart-bar_fill" style={{ height: barFillHeight }}></div>
       </div>
-
       <div className="chart-bar_label">{props.label}</div>
     </div>
   );
